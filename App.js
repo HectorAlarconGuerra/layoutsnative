b2e848e2709ffe82da96445b64886e3964d27025
HectorAlarconGuerra <33630876+HectorAlarconGuerra@users.noreply.github.com>
2020-12-21 19:04:42 -0500

    Scroll Horizontal
    Se borra la imagen cuando le genero

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet, Image, Text, ScrollView} from 'react-native';
 
 const App = () => {
   return (
@@ -11,6 +11,46 @@ const App = () => {
             source={require('./assets/img/bg.jpg')}
           />
         </View>
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>
+            Qué hacer en París
+          </Text>
+          <ScrollView
+            horizontal={true}
+          >
+            <View>
+              <Image
+              style={styles.banner}
+              source={require('./assets/img/actividad1.jpg')}
+              />
+              </View>
+            <View>
+              <Image
+              style={styles.banner}
+              source={require('./assets/img/actividad2.jpg')}
+                />
+              </View>
+            <View>
+              <Image
+              style={styles.banner}
+              source={require('./assets/img/actividad3.jpg')}
+              />
+              </View>
+            <View>
+              <Image
+              style={styles.banner}
+              source={require('./assets/img/actividad4.jpg')}
+              />
+              </View>
+            <View>
+              <Image
+              style={styles.banner}
+              source={require('./assets/img/actividad5.jpg')}
+              />
+              </View>
+          </ScrollView>
+        </View>
+
       </View>
     </>
   );
@@ -21,6 +61,14 @@ const styles = StyleSheet.create({
     height: 250,
     flex: 1,
   },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20
+  },
+  contenedor: {
+    marginHorizontal: 10
+  }
 });
 
 export default App;
